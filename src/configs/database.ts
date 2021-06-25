f524e978d7aa9ab594142d96dfa2cca688f5bc0c
kemuel <kemuellima20@gmail.com>
2021-06-25 10:40:08 -0300

    NLW #06 - Gameplay

---
+++ src/configs/database.ts
@@ -0,0 +1,8 @@
+const DATABASE_NAME = '@gameplay';
+
+const COLLECTION_USERS = `${DATABASE_NAME}:user`;
+const COLLECTION_APPOINTMENT = `${DATABASE_NAME}:appointment`;
+
+export {
+  COLLECTION_USERS, COLLECTION_APPOINTMENT
+}
---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,9 @@
+const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2F%40anonymous%2Fgameplay-41c6290a-07b1-45cb-9113-b27395841155';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '857654706721259571';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+  REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE
+}
